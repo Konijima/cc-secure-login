98419585855411e477100dc0029e03901d7bf22f
Konijima <mathieugermain1991@gmail.com>
2022-05-12 18:59:33 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,4 +1,4 @@
-local url = ''
+local url = 'https://raw.githubusercontent.com/Konijima/cc-secure-login/master/'
 local files = {
     'secure/login.lua',
     'secure/setup.lua',
@@ -6,4 +6,19 @@ local files = {
     'secure/startup.lua',
 }
 
--- incomplete
+local downloads = {}
+for i,file in ipairs(files) do
+    local function download()
+        local script = http.get(url .. file).readAll()
+        if fs.exists(file) then fs.delete(file) end
+        local writer = fs.open(file, 'w')
+        writer.write(script)
+        writer.close()
+    end
+    table.insert(downloads, download)
+end
+
+-- Start
+parallel.waitForAll(table.unpack(downloads))
+
+print('Secure Login installed, run `secure/setup` to secure your computer!')
